f78b70d407c7734af76a82f607c2ca85badf0cec
rafiahmed47 <rafiahmed0471@gmail.com>
2022-03-24 00:17:50 +0600

    adding other folders

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import Navbar from './Components/Navbar/Navbar';
+import Header from './Container/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+          <Navbar/>
+          <Header/>
+
+     hello world
+    </div>
+  );
+}
+
+export default App;
